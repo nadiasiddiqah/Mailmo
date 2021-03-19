db573a04ee4573b6ea1415744be470626ee4c78d
nadiasiddiqah <nadiasiddiqah@gmail.com>
2021-03-19 12:56:44 -0700

    Initial Commit

---
+++ Mailmo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mailmo
+//
+//  Created by Nadia Siddiqah on 3/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MailmoTests/MailmoTests.swift
@@ -0,0 +1,33 @@
+//
+//  MailmoTests.swift
+//  MailmoTests
+//
+//  Created by Nadia Siddiqah on 3/19/21.
+//
+
+import XCTest
+@testable import Mailmo
+
+class MailmoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
