331c3183efb2bb4d7e31cd5219b052db3366da52
nadiasiddiqah <nadiasiddiqah@gmail.com>
2021-05-07 10:53:33 -0700

    Updated gitignore file.

--- Mailmo/View Controllers/SendLater_VC.swift
+++ Mailmo/View Controllers/SendLater_VC.swift
@@ -42,16 +42,5 @@ class SendLater_VC: UIViewController {
             self.performSegue(withIdentifier: "backToMain_Later", sender: nil)
         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
