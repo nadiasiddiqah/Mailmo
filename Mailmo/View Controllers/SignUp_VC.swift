7723035df2d2bbae538f6c7f2210faddc43c2f61
nadiasiddiqah <nadiasiddiqah@gmail.com>
2021-05-06 19:08:40 -0700

    Add .gitignore file

---
+++ Mailmo/View Controllers/SignUp_VC.swift
@@ -0,0 +1,29 @@
+//
+//  SignUp_VC.swift
+//  Mailmo
+//
+//  Created by Nadia Siddiqah on 5/4/21.
+//
+
+import UIKit
+
+class SignUp_VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
