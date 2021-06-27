58b9cd5bd4ae0e7294a35f0ed0cae3b95a0b231f
nadiasiddiqah <nadiasiddiqah@gmail.com>
2021-06-26 17:26:30 -0700

    Update: Change apple and google sign in and sign up icons to compile with guidelines.

--- MailmoUITests/MailmoUITests.swift
+++ MailmoUITests/MailmoUITests.swift
@@ -12,7 +12,6 @@ class MailmoUITests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         let app = XCUIApplication()
-        setupSnapshot(app)
         app.launch()
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
